4b456b291f30c5d05fb149cbe1dc3193e6a9c836
mottalibhassan <145716932+mottalibhassan@users.noreply.github.com>
2024-01-27 17:51:02 +0600

    drink offer

---
+++ freedrinks.js
@@ -0,0 +1,7 @@
+let shoping = 400;
+if ( shoping >= 500){
+    console.log("You have a free coffee")
+}
+else{
+    console.log("Sorry sir you don't have any free coffee you may pay 30 taka for a cup of coffee")
+}
